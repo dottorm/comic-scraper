0acc2d5832f55f49ee2dec8205be086752d9a287
dottorm <marcoparrillo@gmail.com>
2023-01-01 22:10:28 +0100

    remove text, need refactoring

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -8,12 +8,13 @@ import datetime
 import cv2
 from scraper.Scraper import Scraper
 from panel_extractor.PanelExtractor import PanelExtractor
-from text_extractor.TextExtractor import OCR
+from text_extractor.TextExtractor import OCR, inpaint_text
 from utils import get_dates_list, create_dir
 import numpy as np
 
 full_images = "data/full-images"
 splitted_images = "data/splitted-images"
+no_text = "data/no-text"
 
 if __name__ == "__main__":
     # get comic urls
@@ -42,26 +43,16 @@ if __name__ == "__main__":
     ocr = OCR("tesseract")
     transcriptions = ocr.extract_text(splitted_images_paths, clustering=True)
 
-    # visualize
+    create_dir(no_text)
+    # save and visualize
     for path in splitted_images_paths:
-        image = cv2.imread(path) 
+        image = cv2.imread(path)
+        head, tail = os.path.split(path)
         words = transcriptions[path][0]
         bbox = transcriptions[path][1]
         wds = words.split(" ")
-        for box in bbox:
-            x1, y1, x2, y2 = box
-            cv2.rectangle(image,(x1,y1),(x2,y2),(255, 255, 255),cv2.FILLED)
-        imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        imageGrayBlur = cv2.GaussianBlur(imageGray,(5,5),0)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-        imageGrayBlurDilate = cv2.erode(imageGrayBlur, kernel)
-        th, im_th = cv2.threshold(imageGrayBlurDilate, 200, 255, cv2.THRESH_BINARY_INV)
-        hh, ww = im_th.shape[:2]
-        mask = np.zeros((hh+2, ww+2), np.uint8)
-        for box in bbox:
-            x1, y1, x2, y2 = box
-            cv2.floodFill(im_th, mask, (x1,y1), 128);
-            im_th = cv2.bitwise_not(im_th)
-            #fullfilled_balloons = im_th | im_floodfill_inv
-        cv2.imshow("image", im_th)
-        cv2.waitKey(0)
+        img = inpaint_text(image)
+        
+        cv2.imwrite(os.path.join(no_text, tail), img)
+        #cv2.imshow("image", img)
+        #cv2.waitKey(0)
